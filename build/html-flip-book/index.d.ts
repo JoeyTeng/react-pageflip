a62954d32169b115572a26382173c72e2cefdfc6
Joey Teng <joey.teng.dev@gmail.com>
2022-05-12 10:13:35 +0100

    build(2.0.4): build 3

--- build/html-flip-book/index.d.ts
+++ build/html-flip-book/index.d.ts
@@ -1,3 +1,3 @@
 import { PageFlip } from 'page-flip';
 export declare const HTMLFlipBook: any;
-export declare const PageFlipType: typeof PageFlip;
+export declare type PageFlipType = InstanceType<typeof PageFlip>;
